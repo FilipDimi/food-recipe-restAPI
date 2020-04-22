b641bc418fe6e63a20c53d55775c60eae00e1aa2
Filip Dimitrievski <f_dimitrievski@outlook.com>
2020-04-22 18:43:22 -0400

    Added custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
